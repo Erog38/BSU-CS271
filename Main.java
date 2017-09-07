7a226239bdb27bf95b196ad5266e6672a8107f44
jorahsh <30536613+jorahsh@users.noreply.github.com>
2017-09-07 15:23:39 -0600

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,23 @@
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+
+/**
+ * Driver class
+ * 
+ * @author Jorah Hinman
+ *
+ */
+public class Main {
+
+	public static void main(String[] args) {
+		JFrame frame = new JFrame("Welcome to Profile Login!");//make new frame
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		//add a special TraceStateGUI
+		frame.getContentPane().add(new LandingPageGUI());
+		frame.setPreferredSize(new Dimension(800, 800));
+		frame.pack();
+		frame.setVisible(true);
+	}
+
+}
