2579e7962153b4ec9b60fbd5be1a120178ddc32a
Jorah Hinman <Jorah Hinman>
2017-09-14 16:08:21 -0600

    accountmanager update jorah

--- AccountManager.java
+++ AccountManager.java
@@ -9,10 +9,40 @@ public class AccountManager {
         userAccount = new Hashtable<String, UserProfile>();
     }
     
-    public boolean verify(String userName, String password) {
-       if(userName == null || password ==null)    
-    	   return false;   
+    /**
+     * Returns 'true' if username is found in AccountManager
+     * @param username - String
+     * @return found - boolean
+     */
+    public boolean containsUsername(String userName){
+    	boolean found = false;
+    	if(userName != null){
+    		found = userName.equals(userAccount.get(userName));
+    	}
+    	return found;
+    }
+    
+    public boolean verifyEmail(String email){
+    	return false;
+    }
+    
+    public boolean verifyPassword(String userName, String password) {
+       if(userName == null || password ==null){
+    	   return false; 
+       }  
        String storedPassword = userAccount.get(userName).getPassword();
        return storedPassword != null && storedPassword.equals(password);
     }
+    
+    public boolean verifyCreateProfile(UserProfile user){
+    	boolean canCreate = true;
+    	if(containsUsername(user.getName())){
+    		canCreate = false;
+    	}
+    	if(user.getName().length() > 12){
+    		canCreate = false;
+    	}
+    	return canCreate;
+    }
+    
 }
