675bd00aef5c1df7807cfabb28dc13313371f746
Chandra Adhikari <chandraadhikari@u.boisestate.edu>
2017-09-20 15:40:01 -0600

    Added test file for account manager class.

---
+++ AccountManagerTest.java
@@ -0,0 +1,41 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author Chandra Adhikari
+ *
+ */
+public class AccountManagerTest {
+
+	@Test
+	public void testContainsUsername() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testVerifyUserName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testVerifyEmail() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testVerifyPassword() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testVerifyCreateProfile() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testVerifyLogin() {
+		fail("Not yet implemented");
+	}
+
+}
