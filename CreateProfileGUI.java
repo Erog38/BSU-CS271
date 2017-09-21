4bff904e02dcb62ab2092630df50b2a2535dd335
Jorah Hinman <Jorah Hinman>
2017-09-21 11:16:06 -0600

    jorah update

--- CreateProfileGUI.java
+++ CreateProfileGUI.java
@@ -22,8 +22,7 @@ import javax.swing.JTextField;
 public class CreateProfileGUI extends LoginGUI {
 
 	private JPanel mainPanel;
-	private JButton createProfileButton;
-	private JButton exitButton;
+	private JButton returnButton;
 	private JButton signupButton;
 	private ButtonListener buttonListener;
 	private JTextField userNameField;
@@ -44,45 +43,105 @@ public class CreateProfileGUI extends LoginGUI {
 		setLayout(new BorderLayout());
 		mainPanel = new JPanel();
 		mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
-		createSignUpPage(mainPanel);
+		createSignUpPage();
 		add(mainPanel);
 		buttonListener = new ButtonListener();// create button listener
 	}
 
-	private void createSignUpPage(JPanel tmpMainPanel) {
-		tmpMainPanel.setBackground(Color.LIGHT_GRAY);
-		JTextField firstName = new JTextField(10);
-		JTextField lastName = new JTextField(10);
-		JTextField email = new JTextField(15);
-		JTextField userName = new JTextField(10);
-		JTextField password = new JTextField(10);
-		JLabel firstNameLabel = new JLabel("First Name: ");
-		firstNameLabel.setFont(new Font(Font.MONOSPACED, Font.BOLD, 20));
-		JLabel lastNameLabel = new JLabel("Last Name: ");
-		lastNameLabel.setFont(new Font(Font.MONOSPACED, Font.BOLD, 20));
-		JLabel emailLabel = new JLabel("Email: ");
-		emailLabel.setFont(new Font(Font.MONOSPACED, Font.BOLD, 20));
+	private void createSignUpPage() {
+		
+		JPanel centerPanel = new JPanel();
+		
+		centerPanel.add(Box.createVerticalGlue());	
+		mainPanel.add(Box.createVerticalGlue());
+		centerPanel.add(initUserNamePanel());
+		centerPanel.add(initFirstNamePanel());
+		centerPanel.add(initLastNamePanel());
+		centerPanel.add(initEmailPanel());
+		centerPanel.add(initPasswordPanel());
+		centerPanel.add(initButtonPanel());
+		mainPanel.add(centerPanel);		
+		mainPanel.add(Box.createVerticalGlue());
+	}
+	
+	private JPanel initUserNamePanel(){
+		
+		JPanel userNamePanel = new JPanel();
+		
 		JLabel userNameLabel = new JLabel("Username: ");
 		userNameLabel.setFont(new Font(Font.MONOSPACED, Font.BOLD, 20));
+		userNamePanel.add(userNameLabel);
+		JTextField userNameField = new JTextField(20);
+		userNamePanel.add(userNameField);
+		
+		return userNamePanel;
+	}
+	
+	private JPanel initFirstNamePanel(){
+		JPanel firstNamePanel = new JPanel();
+		
+		JLabel firstNameLabel = new JLabel("First name: ");
+		firstNameLabel.setFont(new Font(Font.MONOSPACED, Font.BOLD, 20));
+		firstNamePanel.add(firstNameLabel);
+		JTextField firstNameField = new JTextField(20);
+		firstNamePanel.add(firstNameField);
+		
+		return firstNamePanel;
+	}
+	
+	private JPanel initLastNamePanel(){
+		JPanel lastNamePanel = new JPanel();
+		
+		JLabel lastNameLabel = new JLabel("Last name: ");
+		lastNameLabel.setFont(new Font(Font.MONOSPACED, Font.BOLD, 20));
+		lastNamePanel.add(lastNameLabel);
+		JTextField lastNameField = new JTextField(20);
+		lastNamePanel.add(lastNameField);
+		
+		return lastNamePanel;
+	}
+	private JPanel initEmailPanel(){
+		JPanel emailPanel = new JPanel();
+		
+		JLabel emailLabel = new JLabel("Email: ");
+		emailLabel.setFont(new Font(Font.MONOSPACED, Font.BOLD, 20));
+		emailPanel.add(emailLabel);
+		JTextField emailField = new JTextField(20);
+		emailPanel.add(emailField);
+		
+		return emailPanel;
+	}
+	
+	private JPanel initPasswordPanel(){
+		
+		JPanel passwordPanel = new JPanel();
 		JLabel passwordLabel = new JLabel("Password: ");
 		passwordLabel.setFont(new Font(Font.MONOSPACED, Font.BOLD, 20));
-		signupButton = new JButton("Signup");
-		signupButton.setFont(new Font(Font.MONOSPACED, Font.BOLD, 20));
+		passwordPanel.add(passwordLabel);
+		passwordField = new JTextField(20);
+		passwordPanel.add(passwordField);
+		JLabel confirmLabel = new JLabel("Confirm password: ");
+		confirmLabel.setFont(new Font(Font.MONOSPACED, Font.BOLD, 20));
+		passwordPanel.add(confirmLabel);
+		JTextField confirmField = new JTextField(20);
+		passwordPanel.add(confirmField);
+		return passwordPanel;
+	}
+	
+	private JPanel initButtonPanel(){
+		
+		JPanel buttonPanel = new JPanel();
+		
+		signupButton = new JButton("Create Profile!");
 		signupButton.addActionListener(buttonListener);
-		// editProfileButton.setAlignmentX(Component.CENTER_ALIGNMENT);
-		tmpMainPanel.add(firstNameLabel);
-		tmpMainPanel.add(firstName);
-		tmpMainPanel.add(lastNameLabel);
-		tmpMainPanel.add(lastName);
-		tmpMainPanel.add(emailLabel);
-		tmpMainPanel.add(email);
-		tmpMainPanel.add(userNameLabel);
-		tmpMainPanel.add(userName);
-		tmpMainPanel.add(passwordLabel);
-		tmpMainPanel.add(password);
-		tmpMainPanel.add(signupButton);
-		createNewProfile(firstName.getText(), lastName.getText(), email.getText(), userName.getText(),
-				password.getText());
+		signupButton.setFont(new Font(Font.MONOSPACED, Font.BOLD, 20));
+		buttonPanel.add(signupButton);
+		returnButton = new JButton("Return");
+		returnButton.addActionListener(buttonListener);
+		returnButton.setFont(new Font(Font.MONOSPACED, Font.BOLD, 20));
+		buttonPanel.add(returnButton);
+		
+		return buttonPanel;
 	}
 
 	private void createNewProfile(String first, String last, String email, String usrName, String pass) {
@@ -111,16 +170,6 @@ public class CreateProfileGUI extends LoginGUI {
 		profiles = acct;
 	}
 
-	private void initCenterPanel() {
-
-		JPanel centerPanel = new JPanel();
-
-		centerPanel.add(Box.createVerticalGlue());
-		mainPanel.add(Box.createVerticalGlue());
-		mainPanel.add(centerPanel);
-		mainPanel.add(Box.createVerticalGlue());
-	}
-
 	private class ButtonListener implements ActionListener {
 
 		@Override
@@ -131,11 +180,13 @@ public class CreateProfileGUI extends LoginGUI {
 				String str = JOptionPane.showInputDialog("First Name:", profiles.userAccounts.get(currentlyLoggedIn).getFirstName());
 				System.out.println(str);
 			}
-			if (e.getSource().equals(createProfileButton)) {
-
-			}
-			if (e.getSource().equals(exitButton)) {
-
+			if (e.getSource().equals(returnButton)) {
+				removeAll();
+				add(new LandingPageGUI());
+				repaint();
+				revalidate();
+				setPreferredSize(new Dimension(800, 800));
+			    setVisible(true);
 			}
 		}
 	}
